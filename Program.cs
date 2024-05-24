33f52051e4a52f92f50e0e42e269d8552edffa47
Alan Gabriel <alangabryel13@gmail.com>
2024-05-24 14:33:17 -0300

    Solução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i = new Iphone("+55 (88) 9 1827-6817", "Ruim", "10388478922356", 128);
+Nokia n = new Nokia("+55 (88) 9 3946-1872", "Inquebrável", "39847747459950", 64);
+
+Console.WriteLine("Testando no Iphone:");
+i.Ligar();
+i.InstalarAplicativo("Minecraft");
+i.ReceberLigacao();
+
+Console.WriteLine();
+
+Console.WriteLine("Testando no Nokia:");
+i.Ligar();
+i.InstalarAplicativo("Minecraft");
+i.ReceberLigacao();
